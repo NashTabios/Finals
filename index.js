1708bd28fb3418172ac7f45dce7a0e684d386aeb
NashTabios <fredricknash.tabios.cics@ust.edu.ph>
2024-04-27 17:50:09 +0800

    push commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// Toggle the navbar menu on mobile
+const navbarToggle = document.getElementById('navbar-toggle');
+const navbarMenu = document.getElementById('navbar-menu');
+
+navbarToggle.addEventListener('click', () => {
+    navbarMenu.classList.toggle('active');
+});
