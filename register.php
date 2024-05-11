2c4ed56bdbef87daff8fae53dc4d863d68deb00b
pro-programmershawn <161125241+pro-programmershawn@users.noreply.github.com>
2024-05-11 15:16:40 +0800

    register css ni shawn
    di pa tapos

--- register.php
+++ register.php
@@ -65,42 +65,60 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 
 <body>
+<div class="card">
     <h1>Register</h1>
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-        <div class="form-group">
-            <label>Email Address</label>
-            <input type="text" name="email_address" class="form-control <?php echo (!empty($email_address_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email_address; ?>">
-            <span class="invalid-feedback"><?php echo $email_address_err; ?></span>
-        </div>
-        <div class="form-group">
+    <form class="form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="flex-column">
+                <label>Email</label>
+            </div>
+            <div class="inputForm">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" viewBox="0 0 32 32" height="20">
+                    <g data-name="Layer 3" id="Layer_3">
+                        <path d="m30.853 13.87a15 15 0 0 0 -29.729 4.082 15.1 15.1 0 0 0 12.876 12.918 15.6 15.6 0 0 0 2.016.13 14.85 14.85 0 0 0 7.715-2.145 1 1 0 1 0 -1.031-1.711 13.007 13.007 0 1 1 5.458-6.529 2.149 2.149 0 0 1 -4.158-.759v-10.856a1 1 0 0 0 -2 0v1.726a8 8 0 1 0 .2 10.325 4.135 4.135 0 0 0 7.83.274 15.2 15.2 0 0 0 .823-7.455zm-14.853 8.13a6 6 0 1 1 6-6 6.006 6.006 0 0 1 -6 6z"></path>
+                    </g>
+                </svg>
+                <input placeholder="Enter your Email" class="input" type="text" name="email_address" value="<?php echo $email_address; ?>">
+                <span class="invalid-feedback"><?php echo $email_address_err; ?></span>
+            </div>
+        <div class="flex-column">
             <label>User Name</label>
-            <input type="text" name="user_name" class="form-control <?php echo (!empty($user_name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $user_name; ?>">
+        </div>
+        <div class="inputForm">
+            <input placeholder="Enter your User Name" class="input" type="text" name="user_name" value="<?php echo $user_name; ?>">
             <span class="invalid-feedback"><?php echo $user_name_err; ?></span>
         </div>
-        <div class="form-group">
+        <div class="flex-column">
             <label>First Name</label>
-            <input type="text" name="first_name" class="form-control <?php echo (!empty($first_name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $first_name; ?>">
+        </div>
+        <div class="inputForm">
+            <input placeholder="Enter your First Name" class="input" type="text" name="first_name" value="<?php echo $first_name; ?>">
             <span class="invalid-feedback"><?php echo $first_name_err; ?></span>
         </div>
-        <div class="form-group">
+        <div class="flex-column">
             <label>Last Name</label>
-            <input type="text" name="last_name" class="form-control <?php echo (!empty($last_name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $last_name; ?>">
+        </div>
+        <div class="inputForm">
+            <input placeholder="Enter your Last Name" class="input" type="text" name="last_name" value="<?php echo $last_name; ?>">
             <span class="invalid-feedback"><?php echo $last_name_err; ?></span>
         </div>
-        <div class="form-group">
+        <div class="flex-column">
             <label>Birthday</label>
-            <input type="date" name="dob" class="form-control <?php echo (!empty($dob_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $dob; ?>">
+        </div>
+        <div class="inputForm">
+            <input class="input" type="date" name="dob" value="<?php echo $dob; ?>">
             <span class="invalid-feedback"><?php echo $dob_err; ?></span>
         </div>
-        <div class="form-group">
+        <div class="flex-column">
             <label>Password</label>
-            <input type="password" name="pass" class="form-control <?php echo (!empty($pass_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $pass; ?>">
+        </div>
+        <div class="inputForm">
+            <input placeholder="Enter your Password" class="input" type="password" name="pass" value="<?php echo $pass; ?>">
             <span class="invalid-feedback"><?php echo $pass_err; ?></span>
         </div>
-        <div class="form-group">
-            <input type="submit" class="btn btn-primary" value="Submit">
-            <a href="index.php" class="btn btn-secondary ml-2">Cancel</a>
-        </div>
+        <div class="flex-row">
+                <input type="submit" class="button-submit" value="Submit">
+                <a href="index.php" class="btn btn-secondary ml-2">Cancel</a>
+            </div>
     </form>
 </body>
 
