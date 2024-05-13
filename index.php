8691fc26a9d8645d7861bf663c61549517f4dff5
pro-programmershawn <161125241+pro-programmershawn@users.noreply.github.com>
2024-05-13 17:50:22 +0800

    homepage css
    not yet done

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home - R.E.P.S</title>
+    <link rel="stylesheet" href="index.css">
+
 </head>
 
 <body>
@@ -14,11 +16,14 @@
             <div class="container">
     <script src="index.js"></script>
 </body>
-</html>
-    <p><a href="login.php">login</a></p>
-    <p><a href="register.php">signup</a></p>
+<div class="login-card">
+
+<button class="normal-signin"><a href="login.php">login</a></button>
+<div class="instruction-text">Don't have an Account?</div>
+<button class="create-account"><a href="register.php">Create Account</a></button>
     <p><a href="aboutus.php">about us</a></p>
     <p><a href="adminlogin.php">aDMIN</a></p>
+</div>
 </body>
 
 </html>
