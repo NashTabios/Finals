239d72b65225555ea5c195e6baeb4ec4fde4ca82
pro-programmershawn <161125241+pro-programmershawn@users.noreply.github.com>
2024-05-16 21:54:44 +0800

    homepage done

--- index.php
+++ index.php
@@ -7,44 +7,24 @@
     <link rel="stylesheet" href="index.css">
 </head>
 <body>
-<header id="header">
+<div class="content-wrapper">
 <div class="centered-logo">
     <img src="indexlogo.png" alt="Logo">
 </div>
-    <div class="progressBar-container">
-        <div class="progressBar" id="progressBar"></div>
-    </div>
-    <nav class="navbar">
-        <?php if (isset($user_name)) : ?>
-        <ul class="nav-ul" id="nav_ul">
-            <li><a href="index.php">Home</a></li>
-            <li><a href="aboutus.php">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li><a href="?logout=1">Sign Out</a></li>
-        </ul>
-        <?php else : ?>
-        <ul class="nav-ul" id="nav_ul">
-            <li><a href="index.php">Home</a></li>
-            <li><a href="aboutus.php">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li><a href="login.php">Sign In</a></li>
-        </ul>
-        <?php endif; ?>
-        <div class="hamburger" id="hamburger">
-            <div class="bar1"></div>
-            <div class="bar2"></div>
-            <div class="bar3"></div>
-        </div>
-    </nav>
-</header>
 <div class="cardContainer">
     <div class="login-card">
         <a href="login.php" class="normal-signin">Login</a>
+        <br>
         <div class="instruction-text">Don't have an Account?</div>
         <a href="register.php" class="create-account">Create Account</a>
-        <p><a href="aboutus.php">about us</a></p>
-        <p><a href="adminlogin.php">ADMIN</a></p>
+        <hr class="separator">
+        <div class="admin-about-container">
+        <a href="adminlogin.php" class="admin-link">Admin</a>
+        <span class="vertical-separator"></span>
+        <a href="aboutus.php" class="about-us-link">About us</a>
     </div>
+    </div>
+</div>
 </div>
 <script src="index.js"></script>
 </body>
