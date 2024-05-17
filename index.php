a36506682b3cc6608e2beb36a66c5e5538209a0f
NashTabios <fredricknash.tabios.cics@ust.edu.ph>
2024-05-17 22:41:33 +0800

    now this is some big update on the index

--- index.php
+++ index.php
@@ -1,31 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home - R.E.P.S</title>
-    <link rel="stylesheet" href="index.css">
-</head>
-<body>
-<div class="content-wrapper">
-<div class="centered-logo">
-    <img src="indexlogo.png" alt="Logo">
-</div>
-<div class="cardContainer">
-    <div class="login-card">
-        <a href="login.php" class="normal-signin">Login</a>
-        <br>
-        <div class="instruction-text">Don't have an Account?</div>
-        <a href="register.php" class="create-account">Create Account</a>
-        <hr class="separator">
-        <div class="admin-about-container">
-        <a href="adminlogin.php" class="admin-link">Admin</a>
-        <span class="vertical-separator"></span>
-        <a href="aboutus.php" class="about-us-link">About us</a>
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Home - Index</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="index2.css" />
+    <link rel="stylesheet" href="footer.css" />
+  </head>
+  <body>
+    <aside class="menu">
+      <div class="menu-header">
+        <p>Where to?</p>
+      </div>
+
+      <div class="menu-items">
+        <a href="index.php" class="menu-item">Home</a>
+        <a href="aboutus.php" class="menu-item">About Us</a>
+        <a href="login.php" class="menu-item">Sign-In</a>
+        <a href="register.php" class="menu-item">Sign-Up</a>
+      </div>
+    </aside>
+    <button class="menu-btn">
+      <i class="bi bi-list"></i>
+    </button>
+
+    <div class="container">
+      <nav class="navbar">
+        <div class="nav-logo">
+          <p>REPS</p>
+        </div>
+        <div class="nav-items">
+         <a href="index.php" class="menu-item">Home</a>
+         <a href="aboutus.php" class="menu-item">About Us</a>
+         <a href="login.php" class="menu-item">Sign-In</a>
+         <a href="register.php" class="menu-item">Sign-Up</a>
+        </div>
+      </nav>
+
+      <section class="landing">
+        <h1>first part</h1>
+        <p>lalagay info</p>
+      </section>
     </div>
-    </div>
-</div>
-</div>
-<script src="index.js"></script>
-</body>
+
+    <section class="section">
+      <h1>second part</h1>
+      <p>hindi ko alam ilalagay</p>
+    </section>
+
+    <section class="section">
+      <h1>third section</h1>
+    </section>
+
+    <?php include 'footer.php'; ?>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js"></script>
+    <script src="index2.js"></script>
+  </body>
 </html>
